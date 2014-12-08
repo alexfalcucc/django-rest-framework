8830b67299c4c2e048b70ed2fc7ca81b31e9a3bd
alexfalcucci <alex.falcucci@hotmail.com>
2014-12-08 00:22:27 -0200

    tutorial

---
+++ rest_example/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+from restapp import views
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'rest_example.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^user/', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+)
